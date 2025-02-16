207f1fc25909f5a9b43cbeb20cdae0a478edca02
mbsbuddies <mbsbuddies@gmail.com>
2025-02-16 19:41:07 +1030

    Add use client directive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState, useEffect } from "react";
 import { GoogleMap, LoadScript, Marker, DirectionsRenderer } from "@react-google-maps/api";
 import { Input } from "@/components/ui/input";
